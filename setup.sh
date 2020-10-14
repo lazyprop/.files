778a2a213e160beb4332cd4db1c7922caa7e8b8d
Ishaan Verma <n1trob4ct3r@protonmail.com>
2020-10-14 12:32:30 +0530

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+stow -i -t ~ bash
+stow -i -t ~ x
+stow -i -t ~/.config/neofetch neofetch
+stow -i -t ~/.config/nitrogen nitrogen
+stow -i -t ~/.config/openbox openbox
+stow -i -t ~/.config/nvim nvim
