2c53ac3fed82cde0d689e6a26ff1cfb1b1bb9d21
Ishaan Verma <n1trob4ct3r@protonmail.com>
2020-10-14 17:11:49 +0530

    switch to termite as default terminal emulator

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-stow -i -t ~ bash
-stow -i -t ~ x
-stow -i -t ~/.config/neofetch neofetch
-stow -i -t ~/.config/nitrogen nitrogen
-stow -i -t ~/.config/openbox openbox
-stow -i -t ~/.config/nvim nvim
+stow -v -t ~ bash
+stow -v -t ~ x
+stow -v -t ~/.config/neofetch neofetch
+stow -v -t ~/.config/nitrogen nitrogen
+stow -v -t ~/.config/openbox openbox
+stow -v -t ~/.config/nvim nvim
+stow -v -t /etc/fonts/ fonts
