ee5112da1dfbedb72fe6f96ff8b0e9a67f37a9d0
Ishaan Verma <ishaan56verma@gmail.com>
2020-11-30 11:55:41 +0530

    add rofi

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -323,8 +323,10 @@ globalkeys = gears.table.join(
               end,
               {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+    -- awful.key({ modkey }, "p", function() menubar.show() end,
+              -- {description = "show the menubar", group = "launcher"})
+    -- use demnu instead of awesome menubar
+    awful.key({ modkey }, "p", function() awful.util.spawn_with_shell("dmenu_run") end)
 )
 
 clientkeys = gears.table.join(
