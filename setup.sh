782b0786098d3454b870e479b112fb1c1ec84dcc
Ishaan Verma <ishaan56verma@gmail.com>
2020-12-24 13:17:48 +0530

    remove setup

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-stow -v -t ~ bash
-stow -v -t ~ x
-stow -v -t ~/.config/neofetch neofetch
-stow -v -t ~/.config/nitrogen nitrogen
-stow -v -t ~/.config/openbox openbox
-stow -v -t ~/.config/nvim nvim
-stow -v -t /etc/fonts/ fonts
